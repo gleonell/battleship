4a249a621fed2e3e0a8c186c3eaa1bec2f1a87e5
gleonell <aisaevr999@gmail.com>
2021-04-04 18:53:25 +0300

    added the arrangement of ships as in the original

--- auto_ships_align.cpp
+++ auto_ships_align.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
 #include <cstdlib>
 
-int main(void)
+const int N = 10;
+
+void ships_setting(int map[N][N], int ship_size, int ship_num)
 {
-    while(true)
+    
+    int x,y;
+    int dir = 0;
+    int ship_count = 0;
+
+    while (ship_count < ship_num)
     {
-        const int N = 10;
-        int map[N][N] = {0};
-        int x,y;
-        int dir = 0;
-        int ship_count = 0;
-
-        while (ship_count < 3)
-        {
-            x = rand() % N;
-            y = rand() % N;
-            int temp_x = x;
-            int temp_y = y;
+        x = rand() % N;
+        y = rand() % N;
+        int temp_x = x;
+        int temp_y = y;
         
-            dir = rand() % 4;
+        dir = rand() % 4;
 
-            bool set_is_possible = 1;
+        bool set_is_possible = 1;
 
-            for (int i = 0; i < 5; i++) // проверка возможности простановки 3-x 5-ти палубный кораблей
+        for (int i = 0; i < ship_size; i++) // проверка возможности простановки кораблей
+        {
+            if (x < 0 || y < 0 || x >= N || y >= N)
             {
-                if (x < 0 || y < 0 || x >= N || y >= N)
+                set_is_possible = 0;
+                break;
+            }
+
+            if (map [x  ][y  ] == 1 ||
+                map [x+1][y  ] == 1 ||
+                map [x+1][y+1] == 1 ||
+                map [x  ][y+1] == 1 ||
+                map [x-1][y+1] == 1 ||
+                map [x-1][y  ] == 1 ||
+                map [x-1][y-1] == 1 ||
+                map [x  ][y-1] == 1 ||
+                map [x+1][y-1] == 1 )
                 {
                     set_is_possible = 0;
                     break;
                 }
 
-                if (map [x  ][y  ] == 1 ||
-                    map [x+1][y  ] == 1 ||
-                    map [x+1][y+1] == 1 ||
-                    map [x  ][y+1] == 1 ||
-                    map [x-1][y+1] == 1 ||
-                    map [x-1][y  ] == 1 ||
-                    map [x-1][y-1] == 1 ||
-                    map [x  ][y-1] == 1 ||
-                    map [x+1][y-1] == 1 )
-                    {
-                        set_is_possible = 0;
-                        break;
-                    }
+            switch(dir)
+            {
+                case 0:
+                    x++;
+                break;
+                case 1:
+                    y++;
+                break;
+                case 2:
+                    x--;
+                break;
+                case 3:
+                    y--;
+                break;
+            }
+        }
 
+        if (set_is_possible == 1)
+        {
+            x = temp_x;
+            y = temp_y;
+            for (int i = 0; i < ship_size; i++) // растановка корабля 
+            {
+                map[x][y] = 1;
                 switch(dir)
                 {
                     case 0:
@@ -59,45 +82,35 @@ int main(void)
                         y--;
                     break;
                 }
-            }
-
-            if (set_is_possible == 1)
-            {
-                x = temp_x;
-                y = temp_y;
-                for (int i = 0; i < 5; i++) // растановка корабля 
-                {
-                    map[x][y] = 1;
-                    switch(dir)
-                    {
-                        case 0:
-                            x++;
-                        break;
-                        case 1:
-                            y++;
-                        break;
-                        case 2:
-                            x--;
-                        break;
-                        case 3:
-                            y--;
-                        break;
-                    }
-                } 
-                ship_count++;
-            }
+            } 
+            ship_count++;
         }
+    }
+}
+
+int main(void)
+{
+    while (true)
+    {
+        int map[N][N] = {0};
+
+        ships_setting(map, 4, 1);
+        ships_setting(map, 3, 2);
+        ships_setting(map, 2, 3);
+        ships_setting(map, 1, 4);
+
         for (int i = 0; i < N; i++) // output
         {
             for (int j = 0; j < N; j++)
             {
-                if (map[i][j] == 0)
+                if (map[j][i] == 0)
                     std::cout << '-';
                 else
-                    std::cout << map[i][j];
+                    std::cout << map[j][i];
             }
             std::cout << std::endl;
-        }
-    }    
+        }system("pause");
+    }
+    
     return (0);
 }
